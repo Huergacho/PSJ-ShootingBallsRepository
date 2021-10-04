b33ef76ac27d8f770ba311bc5287cf28124c0f01
Huergacho <75048402+Huergacho@users.noreply.github.com>
2021-10-04 15:34:07 -0300

    Menues

--- Assets/_Scripts/Controllers/GameManager.cs
+++ Assets/_Scripts/Controllers/GameManager.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using System;
 public class GameManager : MonoBehaviour
 {
     public PlayerController mainCharacter;
     public static GameManager instance = null;
     [SerializeField]private bool canStartGame = false;
+    [SerializeField] private bool gamePaused;
+    public bool GamePaused => gamePaused;
     public bool CanStartGame => canStartGame;
     //public SpawnWhenEmpty fullLevelIndicator;
     private void Awake()
@@ -20,7 +23,8 @@ public class GameManager : MonoBehaviour
     }
     private void Update()
     {
-        mainCharacter.gameObject.SetActive(canStartGame);
+        mainCharacter?.gameObject.SetActive(canStartGame);
+
 
     }
     public void IsLevelGenerated(bool hasGenerated)
@@ -29,5 +33,25 @@ public class GameManager : MonoBehaviour
         {
             canStartGame = true;
         }
+    }
+    public void Pause(bool isPaused)
+    {
+        
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+    public void Victory()
+    {
+        SceneManager.LoadScene("Victory");
     }
 }
--- Assets/_Scripts/Controllers/Hud/HudController.cs
+++ Assets/_Scripts/Controllers/Hud/HudController.cs
@@ -3,30 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
-[RequireComponent(typeof (LifeHud),typeof (ScoreManager))]
+[RequireComponent(typeof(LifeHud))]
 public class HudController : MonoBehaviour
 {
     [SerializeField] private Image lifeBar;
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private PlayerController player;
-    [SerializeField] private LifeHud ammoManager;
-    [SerializeField] private ScoreManager scoreManager;
+    [SerializeField] private LifeHud lifeManager;
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button exitButton;
+    [SerializeField] private Button playButton;
     private int actualAmmo;
     private void Start()
     {
+        resumeButton?.onClick.AddListener(Resume);
+        exitButton?.onClick.AddListener(Application.Quit);
+        playButton?.onClick.AddListener(StartGame);
     }
 
     private void Update()
     {
-        if(player == null)
+        if (player == null)
         {
-        player = GameManager.instance.mainCharacter;
+            player = GameManager.instance.mainCharacter;
 
         }
         else
         {
-            ammoManager.ShowLife(lifeBar);
-            scoreManager?.ShowScore(scoreText);
+            lifeManager?.ShowLife(lifeBar);
         }
+        if(pauseMenu != null)
+        CheckPause();
+    }
+    private void CheckPause()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+            GameManager.instance.Pause(true);
+        }
+    }
+    private void Resume()
+    {
+        pauseMenu.SetActive(false);
+        GameManager.instance.Pause(false);
+    }
+    private void StartGame()
+    {
+        GameManager.instance.StartGame();
     }
 }
